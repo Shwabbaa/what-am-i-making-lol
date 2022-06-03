ef7e27e137c95af9e0bd148802ceb1660d0e9e9f
Shwabbaa <65150440+Shwabbaa@users.noreply.github.com>
2022-06-03 19:10:32 +1000

    actually add in the thing i am making

--- main.ts
+++ main.ts
@@ -1,1 +1,41 @@
-// 
+// ok
+basic.showString("if he is hungry press A else press B", 69)
+
+if (blockUntilInput()) {
+    basic.showString("he will eat a meal with meat     does he want it to be spicy? if so press A else press B", 69)
+    if (blockUntilInput()) {
+        basic.showString("He will add chilli", 69)
+    } else {
+        basic.showString("He will add pepper", 69)
+    }
+} else {
+    basic.showString("he will not eat a meal with meat     does he want to eat it with soy sauce? if so press A else press B", 69)
+    if (blockUntilInput()) {
+        basic.showString("He will eat it with rice", 69)
+    } else {
+        basic.showString("He will eat tomato sauce with pasta", 69)
+    }
+}
+
+// input system
+// will block this thread until input
+function blockUntilInput() {
+
+    // show something funny
+    basic.showLeds(`
+    . . # . .
+    . # . # .
+    . . . # .
+    . . . . .
+    . . # . .
+    `)
+
+    while (true) {
+        if (input.buttonIsPressed(Button.A)) {
+            return true
+        } else if (input.buttonIsPressed(Button.B)) {
+            return false
+        }
+    }
+
+}
